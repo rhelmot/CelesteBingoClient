c11b0a15edd4cb5d12b0e6d3b576f131ec872079
Audrey Dutcher <rhelmot@emotionlabs.io>
2023-07-07 17:24:46 +0700

    Update to dotnet core

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
+mkdir -p dist
 dotnet build
 VERSION=$(cat BingoClient/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/BingoClient_${VERSION}${2}.zip
 rm -f $FILENAME
-cd BingoClient/bin/${1-Debug}
-zip -r ../../../${FILENAME} *
+cd BingoClient/bin/net452/${1-Debug}
+zip -r ../../../../${FILENAME} *
 echo Finished in ${FILENAME}
