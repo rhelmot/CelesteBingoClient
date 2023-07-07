a87ab0aee70c47d87d5abdc43a0cbcfbd98688a0
Audrey Dutcher <rhelmot@emotionlabs.io>
2023-07-07 17:27:05 +0700

    lol

--- bundle.sh
+++ bundle.sh
@@ -5,6 +5,6 @@ dotnet build
 VERSION=$(cat BingoClient/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/BingoClient_${VERSION}${2}.zip
 rm -f $FILENAME
-cd BingoClient/bin/net452/${1-Debug}
+cd BingoClient/bin/${1-Debug}/net452
 zip -r ../../../../${FILENAME} *
 echo Finished in ${FILENAME}
