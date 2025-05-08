6cd3f0434b690d6be435607ee1e799c6970f8871
Audrey Dutcher <audrey@rhelmot.io>
2025-05-08 13:44:16 -0700

    bump version to 0.1.16

--- bundle.sh
+++ bundle.sh
@@ -5,6 +5,6 @@ dotnet build
 VERSION=$(cat BingoClient/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/BingoClient_${VERSION}${2}.zip
 rm -f $FILENAME
-cd BingoClient/bin/${1-Debug}/net452
+cd BingoClient/bin/${1-Debug}/net7.0
 zip -r ../../../../${FILENAME} *
 echo Finished in ${FILENAME}
