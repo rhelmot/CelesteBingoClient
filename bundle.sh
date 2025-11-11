e2367ebba68be16087ff3bffec5861417abad1f8
Audrey Dutcher <audrey@rhelmot.io>
2025-11-11 08:21:03 -0700

    Minor bundling fixes

--- bundle.sh
+++ bundle.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 mkdir -p dist
 dotnet build
 VERSION=$(cat BingoClient/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/BingoClient_${VERSION}${2}.zip
 rm -f $FILENAME
-cd BingoClient/bin/${1-Debug}/net7.0
+cd BingoClient/bin/${1-Debug}/net8.0
 zip -r ../../../../${FILENAME} *
 echo Finished in ${FILENAME}
